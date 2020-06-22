eaf61da47d18bc35c1fa944729cebbca51be8363
Spartandie <42554824+Spartandie@users.noreply.github.com>
2020-06-21 22:26:25 -0500

    Delete buscarlibro.h

--- buscarlibro.h
+++
@@ -1,8 +0,0 @@
-#ifndef BUSCARLIBRO_H
-#define BUSCARLIBRO_H
-#include "lista.h"
-
-
-void buscarLista(list *l, char titulo[], char autor[], char editorial[], char isbn[], char formato[], char cantidad[], char precio[]);
-    
-#endif
